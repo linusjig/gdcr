2dcbc5f1254db6876f959eefea1563ea4bc4a942
LIGARC <lina.garcia@thoughtworks.com>
2020-11-13 13:36:49 +0100

    implement game over rules

--- src/test/kotlin/com/lina/kotlin/the_game_of_life/SomeClassTest.kt
+++ src/test/kotlin/com/lina/kotlin/the_game_of_life/SomeClassTest.kt
@@ -2,20 +2,55 @@ package com.lina.kotlin.the_game_of_life
 
 import org.junit.jupiter.api.Test
 import strikt.api.expectThat
-import strikt.assertions.isEqualTo
+import strikt.assertions.isFalse
+import strikt.assertions.isTrue
 
 internal class SomeClassTest {
 
     @Test
-    fun `should return something`() {
-        val someClass = SomeClass()
-        val aNumber = someClass.returnANumber()
-        expectThat(aNumber)
-            .isEqualTo(-1)
+    fun `should return game not finished when grid is empty`(){
+        // given empty grid
+        val grid = """___
+           ___
+           ___    
+        """.trimIndent()
+        val gameWon = false
+        // when check if game is finish
+        val gameOver = isGameOver(grid, gameWon)
+        // then the game is not done
+        expectThat(gameOver).isFalse()
     }
 
-    // Any live cell with fewer than two live neighbours dies, as if caused by under-population.
-    // Any live cell with two or three live neighbours lives on to the next generation.
-    // Any live cell with more than three live neighbours dies, as if by overcrowding.
-    // Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+    @Test
+    internal fun `should return game finished when the grid is full`() {
+        val grid = """OXO
+           XXO
+           OOX    
+        """.trimIndent()
+        val gameWon = false
+        // when check if game is finish
+        val gameOver = isGameOver(grid, gameWon)
+        // then the game is done
+        expectThat(gameOver).isTrue()
+    }
+
+    @Test
+    internal fun `should return game not finish when grid is patially full and nobody has won`() {
+        val grid = """XO_
+           ___
+           ___    
+        """.trimIndent()
+        val gameWon = false
+        // when check if game is finish
+        val gameOver = isGameOver(grid, gameWon)
+        // then the game is not done
+        expectThat(gameOver).isFalse()
+    }
+
+    private fun isGameOver(grid: String, gameWon: Boolean) : Boolean =
+        when {
+            grid.contains("_") -> false
+            gameWon -> true
+            else -> true
+        }
 }
